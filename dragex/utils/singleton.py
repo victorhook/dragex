94ff998a76c627db1cf01fc12f88d3eea6ae7334
victor <victorkrook96@gmail.com>
2021-05-22 18:13:30 +0200

    Fixed singleton to ensure only 1 init method called

--- dragex/utils/singleton.py
+++ dragex/utils/singleton.py
@@ -4,4 +4,10 @@ class Singleton:
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
+            cls._instance.init()
+
         return cls._instance
+
+    def init(self):
+        """ Performs initialization for the Singleton. """
+        pass
