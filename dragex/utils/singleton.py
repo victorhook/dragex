dcff7cfb60fa76b67fe8efe0f9f84feae311f60d
victor <victorkrook96@gmail.com>
2021-06-01 19:26:48 +0200

    added *args and **kwargs for singleton

--- dragex/utils/singleton.py
+++ dragex/utils/singleton.py
@@ -4,10 +4,10 @@ class Singleton:
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance.init()
+            cls._instance.init(*args, **kwargs)
 
         return cls._instance
 
-    def init(self):
+    def init(self, *args, **kwargs):
         """ Performs initialization for the Singleton. """
         pass
