f50cf9c8112be2b38f5c891fbe1939df4ddefa30
victor <victorkrook96@gmail.com>
2021-06-01 15:46:08 +0200

    Added strings to see object state

--- dragex/engine/object_state.py
+++ dragex/engine/object_state.py
@@ -1,4 +1,6 @@
 from engine.animation import AnimationHandler
+from engine.event_queue import EventQueue
+from engine import interact
 
 
 class ObjectState:
@@ -7,13 +9,24 @@ class ObjectState:
     ATTACKING = 2
     DANCING = 3
 
+    _STATE_STRINGS = {
+        IDLE: 'IDLE',
+        MOVING: 'MOVING',
+        ATTACKING: 'ATTACKING',
+        DANCING: 'DANCING'
+    }
+
     def __init__(self, anim_handler: AnimationHandler, state: int = 0):
         self._anim_handler = anim_handler
         self._state = state
+        self._event_queue = EventQueue()
 
     def set(self, state: int) -> None:
         self._state = state
         self._anim_handler.set_state(self._state)
+
+        state_string = self._STATE_STRINGS[self._state]
+        self._event_queue.add(interact.Status('state', state_string))
 
     def get(self) -> int:
         return self._state
