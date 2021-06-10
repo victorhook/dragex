2afa385620c862903c6c9c6741f017d6d3b1629e
victor <victorkrook96@gmail.com>
2021-06-10 20:03:56 +0200

    Started on a generic object factory

---
+++ dragex/engine/object_factory.py
@@ -0,0 +1,58 @@
+from engine.base_object import BaseObject
+from engine.player import Player
+from engine.npc import Npc
+from engine.npc_factory import NpcFactory
+from npcs import Goblin
+import utils
+
+
+def _get_available_game_types() -> list:
+    return [Player, Goblin]
+
+
+class ObjectFactory:
+
+    __AVAILABLE_GAME_TYPES = None
+
+    @staticmethod
+    def serialize(obj: BaseObject) -> dict:
+        """ Serializes a gameobject into a dictionary. """
+        return {
+            'type': obj.__class__.__name__,
+            'name': obj.name,
+            'description': obj.desription,
+            'world_x': obj.position.x,
+            'world_y': obj.position.y,
+            'orientation': obj.position.orientation,
+            'visible': obj.visible,
+            'size': [obj.size.width, obj.size.height],
+        }
+
+    @staticmethod
+    def create(obj: dict) -> BaseObject:
+        """ Creates a game object from the given object dictionary. """
+        ObjectClass = ObjectFactory._get_obj_class(obj['type'])
+
+        if issubclass(ObjectClass, Npc):
+            obj = NpcFactory.create(ObjectClass.__name__)
+        else:
+            obj = ObjectClass(
+                name=obj['name'],
+                description=obj['description'],
+                world_x=obj['world_x'],
+                world_y=obj['world_y'],
+                orientation=obj['orientation'],
+                visible=obj['visible'],
+                size=obj['size']
+            )
+
+        return obj
+
+    @staticmethod
+    def _get_obj_class(obj_t: str) -> type:
+        if ObjectFactory.__AVAILABLE_GAME_TYPES is None:
+            ObjectFactory.__AVAILABLE_GAME_TYPES = _get_available_game_types()
+
+        for obj_type in ObjectFactory.__AVAILABLE_GAME_TYPES:
+            if obj_t.lower() == obj_type.__name__.lower():
+                return obj_type
